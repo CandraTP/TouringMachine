8d3fcddcbd95130392491afc2643d664f1830319
jagad <elarjeigidigiem@gmail.com>
2021-06-20 19:54:20 +0700

    Refactor logic

--- turing.py
+++ turing.py
@@ -1,72 +1,58 @@
 class Tape(object):
-    blank = '_'
+    blankTape = '_'
 
-    def __init__(self, tape_string=''):
-        self._tape = dict(enumerate(tape_string))
+    def __init__(self, initString=''):
+        self._tape = dict(enumerate(initString))
 
     def __str__(self):
-        s = ""
-        min_i = min(self._tape.keys())
-        max_i = max(self._tape.keys())
-
-        for i in range(min_i, max_i+1):
-            s += self._tape[i]
-        return s
-
-    def __getitem__(self, index):
-        if index in self._tape:
-            return self._tape[index]
-        else:
-            return Tape.blank
+        string = ''
+        for i in range(
+                min(self._tape.keys()),
+                max(self._tape.keys()) + 1
+        ):
+            string += self._tape[i]
+        return string
 
     def __setitem__(self, index, value):
         self._tape[index] = value
+
+    def __getitem__(self, index):
+        return self._tape[index] if index in self._tape else Tape.blankTape
 
 
 class TuringMachine(object):
     def __init__(
         self,
         inputTape='',
-        initialState='',
+        initState='',
         finalState=None,
-        transitionFunc=None
+        transFunc=None
     ):
         self._tape = Tape(inputTape)
-        self._headPosition = 0
-        self._blank = Tape.blank
-        self._currentState = initialState
+        self._head = 0
+        self._b = Tape.blankTape
+        self._qc = initState
+        self._tf = {} if transFunc == None else transFunc
+        self._qf = set() if finalState == None else finalState
 
-        if transitionFunc == None:
-            self._transitionFunc = {}
-        else:
-            self._transitionFunc = transitionFunc
-
-        if finalState == None:
-            self._finalState = set()
-        else:
-            self._finalState = finalState
-
-    def get_tape(self):
+    def getTape(self):
         return str(self._tape)
 
-    def step(self):
-        underHead = self._tape[self._headPosition]
-        x = (self._currentState, underHead)
+    def nextMove(self):
+        underHead = self._tape[self._head]
+        x = (self._qc, underHead)
 
-        if x in self._transitionFunc:
-            y = self._transitionFunc[x]
-            self._tape[self._headPosition] = y[1]
+        if x in self._tf:
+            y = self._tf[x]
+            self._tape[self._head] = y[1]
 
             if y[2] == 'R':
-                self._headPosition += 1
+                self._head += 1
             elif y[2] == 'L':
-                self._headPosition -= 1
-            self._currentState = y[0]
+                self._head -= 1
 
+            self._qc = y[0]
             print(self._tape)
 
-    def final(self):
-        if self._currentState in self._finalState:
-            return True
-        else:
-            return False
+    def isFinal(self):
+        return True if self._qc in self._qf else False
